08b1a8e5b84b03973b882068534e3027ee2cee41
miraz66 <mirajulislam0509@gmail.com>
2023-04-20 12:32:07 +0600

    initial api request with axios and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+export type Crypto = {
+  ath: number;
+  atl: number;
+  current_price: number;
+  id: string;
+  name: string;
+  symbol: string;
+  high_24h: number;
+  low_24: number;
+  image: string;
+};
 
 function App() {
+  const [cryptos, setCrytos] = useState<Crypto[] | null>(null);
+
+  useEffect(() => {
+    const url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+
+    axios.get(url).then((respons) => {
+      setCrytos(respons.data);
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="">
+        {cryptos
+          ? cryptos.map((crypto) => {
+              return (
+                <>
+                  <img src={crypto.image} alt="" />
+                  <p>{crypto.name}</p>
+                  <p>{crypto.ath}</p>
+                </>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 }
