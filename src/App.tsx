03b44e45194cf1d5e7b88d683e5271f3fbcfcbd4
miraz66 <mirajulislam0509@gmail.com>
2023-05-02 00:47:30 +0600

    pieChart

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,49 @@ import axios from "axios";
 import CryptoSummary from "./CryptoSummary";
 import { Crypto } from "./Types/Types";
 
+import type { ChartData, ChartOptions } from "chart.js";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import PieChart from "./Types/ChartJs/PieChart";
+import moment from "moment";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 function App() {
   const [cryptos, setCrytos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+
+  const [pieData, setPieData] = useState<ChartData<"pie">>();
+  const [data, setData] = useState<ChartData<"line">>();
+  const [options, setOpetions] = useState<ChartOptions<"line">>({
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  });
 
   useEffect(() => {
     const url =
@@ -23,8 +63,34 @@ function App() {
         <select
           onChange={(e) => {
             const c = cryptos?.find((x) => x.id === e.target.value);
-            console.log(c);
             setSelected(c);
+
+            axios(
+              `https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=1&interval=days`
+            ).then((response) => {
+              console.log(response.data);
+              setData({
+                labels: response.data.prices.map((price: number[]) => {
+                  return moment.unix(price[0] / 1000).format("MM-DD");
+                }),
+                datasets: [
+                  {
+                    label: "Dataset 1",
+                    data: response.data.prices.map((price: number[]) => {
+                      return price[1];
+                    }),
+                    borderColor: "rgb(255, 99, 132)",
+                    backgroundColor: "rgba(255, 99, 132, 0.5)",
+                  },
+                ],
+              });
+
+              setPieData(
+                response.data.prices.map((price: number[]) => {
+                  return price[1];
+                })
+              );
+            });
           }}
           defaultValue="default"
         >
@@ -39,8 +105,27 @@ function App() {
               })
             : null}
         </select>
+        <select
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+          defaultValue="defaultDays"
+        >
+          <option value="defaultDays">Days</option>
+          <option value="">30 days</option>
+          <option value="">15 days</option>
+          <option value="">7 days</option>
+          <option value="">1 days</option>
+        </select>
       </div>
+      <br />
       {selected ? <CryptoSummary crypto={selected} /> : null}
+      {data ? (
+        <div style={{ width: 900, height: 700 }}>
+          <Line options={options} data={data} />
+        </div>
+      ) : null}
+      {pieData ? <PieChart pieData={pieData} /> : null}
     </div>
   );
 }
