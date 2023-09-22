e8a13e3f033e7d057e8a7439e40b5b97e7b5f0aa
miraz66 <mirajulislam0509@gmail.com>
2023-09-22 22:55:33 +0600

    Pie Chart

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -28,6 +28,8 @@ export default function Test() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
     </div>
   );
 }
