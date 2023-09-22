6016c77bbb5a122669815ff2c0cf964e97d26323
miraz66 <mirajulislam0509@gmail.com>
2023-09-22 22:58:04 +0600

    Chart

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -50,6 +50,8 @@ export default function Test() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
     </div>
   );
 }
