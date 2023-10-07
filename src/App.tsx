1252b540dda0bf20757a46f49425b78d4bd51ad8
miraz66 <mirajulislam0509@gmail.com>
2023-10-07 23:53:48 +0600

    SetAmount

--- src/App.tsx
+++ src/App.tsx
@@ -116,9 +116,6 @@ function App() {
     console.log("updateOwned", crypto, amount);
     console.log("updateOwned", crypto, amount);
     console.log("updateOwned", crypto, amount);
-    console.log("updateOwned", crypto, amount);
-    console.log("updateOwned", crypto, amount);
-    console.log("updateOwned", crypto, amount);
   }
 
   return (
