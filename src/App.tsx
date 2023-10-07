c1e32e49e14db023623cc0e51968aa3395871f45
miraz66 <mirajulislam0509@gmail.com>
2023-10-07 23:52:21 +0600

    Summary

--- src/App.tsx
+++ src/App.tsx
@@ -117,6 +117,8 @@ function App() {
     console.log("updateOwned", crypto, amount);
     console.log("updateOwned", crypto, amount);
     console.log("updateOwned", crypto, amount);
+    console.log("updateOwned", crypto, amount);
+    console.log("updateOwned", crypto, amount);
   }
 
   return (
